3373350ef2b007b4120104a0b0cff8704b977d5e
Badral <xenonlite6@gmail.com>
2023-11-13 22:47:15 +0800

    main.py_update

--- main.py
+++ main.py
@@ -26,13 +26,26 @@ def get_movie_by_rating():
 
     return response["results"]
 
+def get_genre_name():
+    url = "https://api.themoviedb.org/3/genre/movie/list"
+
+    headers = {
+        "accept": "application/json",
+        "Authorization": "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjYzNjODc5MThiYjA3Mjk5ZjAzY2NlNGU0ZDA3MTFiZSIsInN1YiI6IjY1NGNhM2QzYjE4ZjMyMDBhYzNlZDFiNCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.XANKZdfeoc99yHfwNK85aDgZnRRSHWr2x-SuFKv8s4Q"
+    }
+
+    response = requests.get(url, headers=headers).json()
+
+    return response["genres"]
+
 def save_data(data):
     client = pymongo.MongoClient("mongodb://localhost:27017/")
     database = client["Web_system"]  # Replace 'your_database_name' with your actual database name
-    collection = database["BD"]  # Replace 'your_collection_name' with your actual collection name
+    collection = database["genre"]  # Replace 'your_collection_name' with your actual collection name
     
     for i in range(len(data)):
         collection.insert_one(data[i])
 
-data = get_movie_by_rating()
+# data = get_movie_by_rating()
+data = get_genre_name()
 save_data(data)

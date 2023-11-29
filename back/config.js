1a9200620c34acae061a8eedf9d0da75ae55e391
Badral <xenonlite6@gmail.com>
2023-11-29 23:38:44 +0800

    MongoDB data retrieval success

--- back/config.js
+++ back/config.js
@@ -1,3 +1,5 @@
 export const PORT = 3000;
 
 export const mongoDBURL = "mongodb://127.0.0.1/Web_system"
+
+export const mongoDBURLL = "mongodb://127.0.0.1"
--- back/index.js
+++ back/index.js
@@ -1,44 +1,45 @@
 import express from "express";
-import { PORT, mongoDBURL } from "./config.js";
+import { PORT, mongoDBURL, mongoDBURLL } from "./config.js";
 import mongoose, { SchemaType } from "mongoose";
-import { error } from "console";
-import schemaa from "./product.cjs"
+import {MongoClient} from "mongodb";
+import movie from "./product.cjs";
 // const schemaa = require("./product.cjs")
 
 const app = express();
 app.use(express.json());
-
-try{
-    let data = schemaa.find().limit(1);
-    console.log(data)
-}
-catch{
-    console.log("Aldaa!")
-}
-
-
-
-
-
-
-
-
-
+const client = new MongoClient(mongoDBURLL);
 
 // app.get("/", async (request,response)=>{
-//     let data = await schemaa.find();
-//     response.send(data);
-// });
-
-
-
-// mongoose.connect(mongoDBURL)
-//     .then(() => {
-//         console.log("App connected to DB");
-//         app.listen(PORT,()=> {
-//         console.log("Runningshdee port: ",PORT);
-//     });
-//     })
+//     mongoose.connect(mongoDBURL)
+//     .then(
+//         movie.find({}).limit(20)
+//         .then(data => {
+//         console.log('Retrieved movies:', data);
+//         // Close the database connection after retrieving the data
+//         response.send(data)
+//         mongoose.connection.close();
+//     }))
 //     .catch((error) =>{
 //         console.log(error);
 //     })
+// });
+try {
+    // Connect to the MongoDB database
+    await client.connect();
+
+    // Access the specific collection (replace 'your_collection_name' with your actual collection name)
+    const collection = client.db('Web_system').collection('BD');
+
+    // Fetch the first 20 documents from MongoDB
+    const data = await collection.find().limit(1).toArray();
+    console.log(data)
+  } catch (error) {
+    console.error('Error fetching data from MongoDB:', error);
+  } finally {
+    // Close the MongoDB connection
+    await client.close();
+  }
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
